5f00199d8e213527f2fd0f8f455b66e94d63e943
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2025-12-27 18:02:10 -0300

    fixação de estudos

--- Aula08/Program.cs
+++ Aula08/Program.cs
@@ -6,7 +6,24 @@ namespace Aula08
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.ReadLine(); serve para fazer a leitura dos valores do teclado
+
+            string nome;
+
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("Olá, " + nome + "! Seja bem-vindo(a) ao C#.");
+
+            int number1, number2, soma;
+
+            //int.parce() converte uma string em um número inteiro.
+
+            Console.Write("Digite o primeiro número: ");
+            number1 = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            number2 = int.Parse(Console.ReadLine());
+            soma = number1 + number2;
+            Console.WriteLine("A soma dos números é: " + soma);
         }
     }
 }
--- TestAulas/Program.cs
+++ TestAulas/Program.cs
@@ -6,7 +6,36 @@ namespace TestAulas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int codigo, quantidade;
+            double lucro, valor;
+            string produto;
+
+
+            Console.WriteLine("---LK IMPORTADORA---");
+
+            Console.Write("Digite o código do produto: ");
+            codigo = int.Parse(Console.ReadLine());
+            Console.Write("Digite o nome do produto: ");
+            produto = Console.ReadLine();
+            Console.Write("Digite o valor de compra: ");
+            valor = double.Parse(Console.ReadLine());
+            Console.Write("Digite a quantidade comprada: ");
+            quantidade = int.Parse(Console.ReadLine());
+            Console.Write("Digite o lucro desejado (em decimal): ");
+            lucro = double.Parse(Console.ReadLine());
+            double valorVenda = valor + (valor * lucro);
+
+
+            Console.WriteLine("\n--- NOTA FISCAL ---");
+            Console.WriteLine("Código do produto.......:{0,15}", codigo);
+            Console.WriteLine("Nome do produto.........:{0,15}", produto);
+            Console.WriteLine("Valor de compra.........:{0,15:c}", valor);
+            Console.WriteLine("Quantidade comprada.....:{0,15}", quantidade);
+            Console.WriteLine("Lucro desejado..........:{0,15:p}", lucro);
+            Console.WriteLine("Valor de venda..........:{0,15:c}", valorVenda);
+            Console.WriteLine("Total da compra.........:{0,15:c}", valorVenda * quantidade);
+            Console.WriteLine("-------------------");
+
         }
     }
 }
