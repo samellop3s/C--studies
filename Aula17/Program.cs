f41c24e2ea32fd9153b3aab626d2910996f6e170
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-01-20 21:36:29 -0300

    testando conhecimento

--- Aula17/Program.cs
+++ Aula17/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 
 namespace Aula17
 {
@@ -8,17 +10,18 @@ namespace Aula17
         {
 
             //Arrays / vetores
-            int [] n = new int[5];
+            int[] n = new int[5];
 
             //int[] num = new int[3]{55, 65, 75};
 
-            int []  num = {55, 65, 75};
+            int[] num = { 55, 65, 75 };
+
 
             //sempre lembrar que o primeiro elemento do array começa no índice 0
             //então um array de 5 posições vai do índice 0 ao índice 4
             //atribuindo valores ao array
             //a quantidade de arrey é considerado pela quantidade de elementos
-            
+
             //sempre lembrar que podemos ter varios tipos de arrays, sendo eles inteiros , string, double, char, etc.
             //sempre q for iniciar um array, devemos informar o tipo, dps colocar os colchetes [] e depois o nome do array
             string[] veiculos = new string[3];
@@ -32,9 +35,60 @@ namespace Aula17
             n[2] = 30;
             n[3] = 40;
             n[4] = 50;
-            
+
             Console.WriteLine(num[2]);
-            
+
+            //1° forma de usar o laço de repetição FOR
+            int[] number = { 100, 200, 300, 400, 500 };
+
+            int soma = 0;
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                soma += number[i];
+            }
+
+            double media = soma / number.Length;
+
+            Console.WriteLine("A soma é: " + soma);
+            Console.WriteLine("A média é: " + media);
+
+            //2°
+
+            int[] valores = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+
+            int maior = valores[0];
+            int menor = valores[0];
+
+            for (int i = 1; i < valores.Length; i++)
+            {
+                if (valores[i] > maior)
+                {
+                    maior = valores[i];
+                }
+                else if (valores[i] < menor)
+                {
+                    menor = valores[i];
+                }
+            }
+
+            Console.WriteLine("O maior valor é: " + maior);
+            Console.WriteLine("O menor valor é: " + menor);
+
+            //3°
+
+            string[] frutas = { "Maçã", "Banana", "Laranja", "Uva", "Abacaxi" };
+            /*Crie um vetor de 6 números inteiros.
+
+Objetivos:
+
+Mostrar o vetor original
+
+Mostrar o vetor invertido (do último pro primeiro)
+
+Exemplo:
+Entrada: [1, 2, 3, 4, 5, 6]
+Saída invertida: [6, 5, 4, 3, 2, 1]*/
         }
     }
 }
