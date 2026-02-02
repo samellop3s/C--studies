8ff817f4f81948755f51b5c502dd50d70cae1a40
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-02-02 19:28:06 -0300

    test

--- sabadoKamilly/Program.cs
+++ sabadoKamilly/Program.cs
@@ -1,16 +1,49 @@
 ﻿using System;
+using System.Threading.Tasks.Dataflow;
 
-namespace sabadoKamilly
+namespace AppTest
 {
     class Program
     {
-        enum DiasSemana { Domingo, Segunda, Terca, Quarta, Quinta, Sexta, Sabado };
         static void Main(string[] args)
         {
-            Console.Write("Em qual dia da semana você gostaria de agendar sua consulta? ");
-            string entrada = Console.ReadLine();    
-            DiasSemana diaConsulta = Enum.Parse<DiasSemana>(entrada);
-            Console.WriteLine("Sua consulta foi agendada para: " + diaConsulta);
+            int[] number = { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Vetores na ordem correta: ");
+            for (int i = 0; i < number.Length; i++)
+            {
+                Console.Write(number[i] + " ");
             }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Vetores ao contrario: ");
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                Console.Write(number[i] + " ");
+            }
+
+            string[] frutas = {"Banana", "Pera", "UVa", "Laranja", "Tomate"};
+
+            bool encontrou = false;
+
+            Console.WriteLine("Nesta cesta tem a Banana?");
+
+            foreach(string fruta in frutas)
+            {
+                if(fruta == "Banana")
+                {
+                    encontrou = true;
+                }
+            }
+            if (encontrou)
+            {
+                Console.WriteLine("OBAAAA, vc encontrou a banana!!!!");
+            }
+            else
+            {
+                Console.WriteLine("Poxa, vc n encontrou a banana");
+            }
+        }
     }
 }
