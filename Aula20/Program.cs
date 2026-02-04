6ae864fe98b75d1a1b481f2f410b4dc44d89cc47
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-02-03 22:44:49 -0300

    aula20

---
+++ Aula20/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AppTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] number = new int [10];
+
+            for(int i = 0; i<number.Length; i++)
+            {
+                number[i]=i;
+            }
+
+            for(int i=0; i<number.Length; i++)
+            {
+                Console.WriteLine("Valor de num na pos{0}: {1}",i,number[i]);
+            }
+        }
+    }
+}
