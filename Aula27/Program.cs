af3a70f1f4d5522fdf46705ff412096a5b5fbe0f
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-07-02 22:44:49 -0300

    add comentarios

--- Aula27/Program.cs
+++ Aula27/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+
+namespace Aula27
+{
+    /*a passagem de valor no formato params, permite fazer a passagem de mais de um pparametro sem precisar indicar eles no metodo */
+    class Program
+    {
+        static void Main()
+        {
+            
+        }
+        static void Soma(int number1, int number2)
+        {
+             
+        }
+    }
+}
