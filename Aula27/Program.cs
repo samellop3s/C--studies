2a4256e4f4e1c13c629e47bae77ff5087c0a97b6
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-07-02 23:19:50 -0300

    teste do codigo

--- Aula27/Program.cs
+++ Aula27/Program.cs
@@ -7,11 +7,27 @@ namespace Aula27
     {
         static void Main()
         {
-            
+            Soma(10, 25, 36, 45);
         }
-        static void Soma(int number1, int number2)
+        static void Soma(params int[]n)
         {
-             
+            int res = 0;
+
+            if(n.Length < 1)
+            {
+                Console.WriteLine("Não existem valores a serem somados");
+            }else if(n.Length < 1)
+            {
+                Console.WriteLine($"Valores insuficientes para soma : {n}");
+            }
+            else
+            {
+                for(int i=0; i<n.Length; i++)
+                {
+                    res+=n[i];
+                }
+                Console.WriteLine($"A Soma dos valores é: {res}");
+            }
         }
     }
 }
